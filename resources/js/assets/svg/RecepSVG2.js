0828d1985833a31e4fb93b3450a207ae2f0c0086
Demola Malomo <dev@saedintservices.com>
2019-09-17 15:38:38 +0100

    Workers PAge Update

---
+++ resources/js/assets/svg/RecepSVG2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RecepSVG2 = props => (
+  <svg fill="none" {...props}>
+    <path
+      d="M250-1H0v180.42c5.013 109.756 223.058 142.331 242.481 53.124C250 159 229.5 176.5 228.19 123.779c0-27.279 19.676-35.29 21.81-59.126V-1z"
+      fill="#DBE5FF"
+      fillOpacity={0.5}
+    />
+  </svg>
+);
+
+export default RecepSVG2;
