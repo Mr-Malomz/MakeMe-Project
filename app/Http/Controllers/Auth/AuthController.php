433566febbafe1a4296a18ff988f4932eb18e717
sahjome <sahjome@gmail.com>
2019-10-09 11:41:13 +0100

    wait

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    //Endpoint to create job
     public function createJobCard(Request $request)
     {
         $card = DB::select('call spCreateJobCard(?, ?, ?, ?, ?, ?, ?)', [
@@ -54,30 +55,7 @@ class AuthController extends Controller
         102, $request->phone]);
     }
 
-    public function Employee(Request $request)
-    {
-        $pass = Hash::make($request->password);
-        $pro = DB::insert('call spMakeMeEmp (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
-            $request->Transtype,
-            $request->title,
-            $request->surn,
-            $request->fname,
-            $request->midnme,
-            $pass,
-            $request->dob,
-            $request->email,
-            $request->phone,
-            $request->addr,
-            $request->salary,
-            $request->comm,
-            $request->role,
-            $request->oldpass
-        ]);
-        return response()->json($emp);
-        
-    }
-
-    //Endpoint to edit customer
+    //Endpoint to edit customer details
     public function EditCustomer(Request $request)
     {
         $emp = DB::insert('call spMakeMePro (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
@@ -98,5 +76,47 @@ class AuthController extends Controller
         ]);
         return response()->json($emp);
         
+    }
+
+    //Endpoint to add employee
+    public function Employee(Request $request)
+    {
+        $pass = Hash::make($request->password);
+        $pro = DB::insert('call spMakeMeEmp (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+            1001,
+            $request->title,
+            $request->surn,
+            $request->fname,
+            $request->midnme,
+            $pass,
+            $request->dob,
+            $request->email,
+            $request->phone,
+            $request->addr,
+            $request->salary,
+            $request->comm,
+            $request->role,
+            $request->oldpass
+        ]);
+        return response()->json($pro);
+    }
+
+    //Endpoint for employee login
+    public function LoginEmp(Request $request)
+    {
+        $pass = Hash::make($request->password);
+        $pro = DB::select('call spMakeMeEmp (?, ?)',[
+            1002,
+            $pass,
+            $request->email
+        ]);
+        return response()->json($pro);
+    }
+    
+    //Enpoint for employee to change password
+    public function ChangePassword()
+    {
+        $pass = Hash::make('C0dename47');
+        return response()->json($pass);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::any('/password/{password}','Auth\AuthController@Hasher');
